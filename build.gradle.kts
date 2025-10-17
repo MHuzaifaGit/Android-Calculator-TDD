68fbb164600c770a9b861ca293c2b2a0a0c4b406
Huzaifa <huzaifa3082004@gmail.com>
2025-10-17 19:44:59 +0500

    Save local updates: legacy fixes and project updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,10 @@
 // Top-level build file
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-    }
-}
+// buildscript block no longer needed with plugins DSL above
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
